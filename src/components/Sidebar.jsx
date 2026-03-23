c7d57d0049cb28b03c34db8c444bcde4598a9857
mihirs <mihirsonar312@gmail.com>
2026-03-23 15:56:50 +0530

    fix 2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { Link } from "react-router-dom";
 
-export default function Sidebar() {
+export default function sidebar() {
   return (
     <aside className="w-60 bg-white border-r p-4">
 
